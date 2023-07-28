88b36b25b44a73449cb0e8e6965c7cb9d5173d9a
Dukeinfo <work@dukeinfosys.com>
2023-07-28 22:11:51 +0530

    changes added

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -14,13 +14,9 @@ class CreatePersonalAccessTokensTable extends Migration
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
-  
             $table->bigIncrements('id');
-            $table->string('tokenable_type', 191);
-            $table->unsignedBigInteger('tokenable_id');
-            $table->index(['tokenable_type', 'tokenable_id']);
             $table->morphs('tokenable');
-            $table->string('name');
+            $table->string('name', 191);
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
