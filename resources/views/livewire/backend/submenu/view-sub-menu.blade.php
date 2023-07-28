322e4510e73b142c52a2d8adeed05df590ebe7b6
Dukeinfo <work@dukeinfosys.com>
2023-07-29 00:04:19 +0530

    thumbnail fixed

--- resources/views/livewire/backend/submenu/view-sub-menu.blade.php
+++ resources/views/livewire/backend/submenu/view-sub-menu.blade.php
@@ -220,7 +220,7 @@
                                         </td>
                                             <td> {{$record->Menu->name ?? '' }}</td>
                                             <td> 
-                                                <img src="{{ asset('storage/thumbnail').'/'.$record->image }}" alt="Image" width="100" height="70"/>
+                                                <img src="{{ asset('uploads/thumbnail').'/'.$record->thumbnail }}" alt="Image" width="100" height="70"/>
                                             
                                             </td>
                                             <td>{{$record->name ?? '' }}</td>
