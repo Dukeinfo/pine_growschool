f2bb00eb6253e3590ead43dc545829ee4cea6cbc
Dukeinfo <work@dukeinfosys.com>
2023-08-25 16:26:14 +0530

    roll of honour done@jasvir

--- app/Models/Rollofhonour.php
+++ app/Models/Rollofhonour.php
@@ -6,14 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\CategoryHonour;
-
+use App\Models\SubCategoryHonour;
 class Rollofhonour extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
-     public function gelHonourCategory()
+     public function gelCategory()
     {
         return $this->belongsTo(CategoryHonour::class,'category_honour_id');
+    }
+
+     public function gelSubCategory()
+    {
+        return $this->belongsTo(SubCategoryHonour::class,'sub_category');
     }
 }
